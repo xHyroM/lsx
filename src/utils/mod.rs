5fc1c65a20cdecd69343713c373568c662be3812
Fire-The-Fox <gajdos.jan77@gmail.com>
2023-03-15 18:27:44 +0100

    fixed size units

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,5 @@
 pub fn format_size(size: u64) -> String {
-    let units = ["B", "KB", "MB", "GB", "TB", "PB", "EB"];
+    let units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"];
     let mut size = size as f64;
     let mut i = 0;
     while size >= 1024.0 && i < units.len() - 1 {
